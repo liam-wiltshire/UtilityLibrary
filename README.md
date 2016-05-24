2b24aa4290ba3c0cd1a8014910075367dd151eab
Absolute Design <absolutedesign@Absolutes-MacBook-Pro.local>
2016-05-24 14:09:44 +0100

    Inital functionality / description

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # UtilityLibrary
 Small JavaScript utility library for quick reference functions without jQuery
+
+## Demo
+https://jsfiddle.net/m4mf3qx1/1/
+
+## Examples
+
+### Extend
+```javascript
+$.fn.foo = function() {
+	// Function here
+}
+```
+
+### Select
+```javascript
+var elementByClass = $('.selector');
+var elementById = $('#selector');
+```
+
+### Add class
+```javascript
+$('.selector').addClass('class');
+```
+
+### Remove class
+```javascript
+$('.selector').removeClass('class');
+```
+
+### Toggle class
+```javascript
+$('.selector').toggle('class');
+```
+
+### Chaining
+```javascript
+$('.selector').addClass('class').removeClass('class');
+```
---
+++ utility.js
@@ -0,0 +1,61 @@
+'use strict';
+
+(function(window, undefined){
+
+	// Return an instance of the UtilityLibrary object
+	var absoluteUtility = window.$ = function(selector) {
+		if(!(this instanceof UtilityLibrary)) {
+			return new UtilityLibrary(selector);
+		}
+	}
+
+	var UtilityLibrary = function(selector) {
+		// Selector implementation
+		var nodes = document.querySelectorAll(selector);
+
+		// Store each element to the UtilityLibrary object
+		for (var i = 0; i < nodes.length; i++) {
+			this[i] = nodes[i];
+		}
+
+		// Store reference of the length to the object
+		this.length = nodes.length;
+
+		// Return this to allow chaining
+		return this;
+	};
+
+	// Expose the prototype object via AbsoluteUtility.fn so methods can easily be added
+	absoluteUtility.fn = UtilityLibrary.prototype = {
+		// API methods
+		// Return this from each method to allow chaining
+		
+		// Method to add a class to a selected element
+		addClass: function(classToAdd) {
+			for (var i = 0; i < this.length; i++) {
+				this[i].classList.add(classToAdd);
+			}
+
+			return this;
+		},
+
+		// Method to remove a class from a selected element
+		removeClass: function(classToRemove) {
+			for (var i = 0; i < this.length; i++) {
+				this[i].classList.remove(classToRemove);
+			}
+
+			return this;
+		},
+
+		// Method to toggle a class on a selected element
+		toggle: function(classToToggle) {
+			for (var i = 0; i < this.length; i++) {
+				this[i].classList.toggle(classToToggle);
+			}
+
+			return this;
+		}
+	}; 
+
+})(window);
